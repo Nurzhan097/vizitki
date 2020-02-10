9e568c2dfe78b6d83b00f5a3b57b3e73c377cac3
Nurzhan097 <nnnurjan097@gmail.com>
2020-02-11 00:15:14 +0600

    'heroku7'

--- VizitkiAPIProject/settings.py
+++ VizitkiAPIProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -164,7 +165,7 @@ REST_FRAMEWORK = {
 }
 
 SITE_ID = 1
-
+django_heroku.settings(locals())
 
 
 
